fbdc860fcfdcdbc250608b449d3e660ede86ff63
HanXiao <Yipartner@1350690718@qq.com>
2018-06-18 12:31:52 +0800

    catalog数据库迁移

---
+++ database/migrations/2018_06_09_224123_create_catalogs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCatalogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('catalogs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->tinyInteger('lv');
+            $table->integer('parent_id');
+            $table->text('content')->nullable();
+            $table->dateTime('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('catalogs');
+    }
+}
